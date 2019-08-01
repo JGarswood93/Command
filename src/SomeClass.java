f43bf257277333baffdec7c0d20bd06f7a8a950d
Jake <jacobbarton@rentalcars.com>
2019-08-01 14:24:56 +0100

    first commit

---
+++ src/SomeClass.java
@@ -0,0 +1,12 @@
+public class SomeClass {
+
+
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.out.println("no arguments");
+        } else {
+            System.out.println(args.length + " arguments");
+        }
+    }
+}
+
